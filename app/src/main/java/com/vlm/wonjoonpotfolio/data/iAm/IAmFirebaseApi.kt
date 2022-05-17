62afa742b95eb1f94b7a6c2bba9f94e85d81fcf7
doojoons@naver.com <doojoons@naver.com>
2022-05-17 21:06:20 +0900

    sdd

--- app/src/main/java/com/vlm/wonjoonpotfolio/data/iAm/IAmFirebaseApi.kt
+++ app/src/main/java/com/vlm/wonjoonpotfolio/data/iAm/IAmFirebaseApi.kt
@@ -2,10 +2,12 @@ package com.vlm.wonjoonpotfolio.data.iAm
 
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 
 class IAmFirebaseApi(private val firebase : FirebaseFirestore) : IAmRemoteDataSource {
-    override suspend fun getIam(): Flow<IAm> {
+    override suspend fun getIam(): Flow<IAm> = flow{
         val mainDoc = firebase
+
     }
 }
