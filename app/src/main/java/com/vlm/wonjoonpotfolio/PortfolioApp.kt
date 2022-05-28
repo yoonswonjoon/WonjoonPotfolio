7564f2553e273d163c1e47d3b46c13c2942efcd0
dooju <doojoons@naver.com>
2022-05-29 02:54:38 +0900

    로그인을 구현하여야 댓글달기가 가능함.

--- app/src/main/java/com/vlm/wonjoonpotfolio/PortfolioApp.kt
+++ app/src/main/java/com/vlm/wonjoonpotfolio/PortfolioApp.kt
@@ -1,8 +1,13 @@
 package com.vlm.wonjoonpotfolio
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class PortfolioApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSdk.init(this, "e7637755be0d120fa7bde2428d272f0c")
+    }
 }
